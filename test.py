6827f02ecca85d8ac44cee9ddd858e64185170d0
leLoir <leLoir@bitbucket.org>
2018-03-25 10:14:54 -0400

    encodage + longs kws

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
+# -*- coding: latin-1 -*-
 from gensim.summarization import keywords
 from polyglot.text import Text
 
 
 def extract_keywords(text):
-    kw = keywords(text, ratio=0.1, lemmatize=True).split('\n')
+    kw = keywords(text, ratio=0.5, lemmatize=True).split('\n')
     str1 = ' '.join(kw)
     text = Text(str1, hint_language_code='fr')  # switch to fr
     filtre = text.pos_tags
@@ -11,10 +12,13 @@ def extract_keywords(text):
     resultat = []
 
     for word in filtre:
-        if word[1] == 'NOUN':
+        if word[1] == 'NOUN' and len(word[0]) > 4:
             resultat.append(word[0])
+    #print(resultat)
 
-    print(resultat)
+
+file = open("histoire.txt", "r", encoding='latin-1')
+histoire = file.read()
 
 text = "Salut je parle de montagne de loup et de nature"
-extract_keywords(text)
+extract_keywords(histoire)
